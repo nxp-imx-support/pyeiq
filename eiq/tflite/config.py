b6d1898d6abccbb187318e6d29a2b864269493f7
Diego Dorta <diego.dorta@nxp.com>
2020-04-14 20:19:52 -0300

    Add config file for tflite class
    Signed-off-by: Diego Dorta <diego.dorta@nxp.com>

---
+++ eiq/tflite/config.py
@@ -0,0 +1,9 @@
+
+OBJECT_RECOGNITION_MODEL = "https://storage.googleapis.com/download.tensorflow.org/models/tflite/coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip"
+
+LABEL_IMAGE_MODEL = "https://storage.googleapis.com/download.tensorflow.org/models/mobilenet_v1_2018_02_22/mobilenet_v1_1.0_224.tgz"
+LABEL_IMAGE_LABELS = "https://storage.googleapis.com/download.tensorflow.org/models/mobilenet_v1_1.0_224_frozen.tgz"
+LABEL_IMAGE_DEFAULT_IMAGE = "https://raw.githubusercontent.com/tensorflow/tensorflow/master/tensorflow/lite/examples/label_image/testdata/grace_hopper.bmp"
+
+FIRE_DETECTION_MODEL = "https://github.com/ARM-software/ML-examples/raw/master/pyarmnn-fire-detection/fire_detection.tflite"
+FIRE_DETECTION_DEFAULT_IMAGE = "https://unsplash.com/photos/7WNEiyeMbB0/download?force=true&w=640"
