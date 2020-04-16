ab4eb0bc463205cc5bdb21207272a902bced8aa1
Alifer Moraes <alifer.moraes@nxp.com>
2020-04-16 16:00:26 -0300

    Add model url for the new demo Camera_OpenCV
    Signed-off-by: Alifer Moraes <alifer.moraes@nxp.com>

--- eiq/tflite/config.py
+++ eiq/tflite/config.py
@@ -7,3 +7,5 @@ LABEL_IMAGE_DEFAULT_IMAGE = "https://raw.githubusercontent.com/tensorflow/tensor
 
 FIRE_DETECTION_MODEL = "https://github.com/ARM-software/ML-examples/raw/master/pyarmnn-fire-detection/fire_detection.tflite"
 FIRE_DETECTION_DEFAULT_IMAGE = "https://unsplash.com/photos/7WNEiyeMbB0/download?force=true&w=640"
+
+CAMERA_OPENCV_MODEL = "https://dl.google.com/coral/canned_models/all_models.tar.gz"
