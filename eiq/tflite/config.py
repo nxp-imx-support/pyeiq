5a880fec29485773f737cbb0083ea939a122cd2f
Alifer Moraes <alifer.moraes@nxp.com>
2020-04-17 18:17:30 -0300

    Add new constants for ObjectDetectionOpenGStreamer demo
    Signed-off-by: Alifer Moraes <alifer.moraes@nxp.com>

--- eiq/tflite/config.py
+++ eiq/tflite/config.py
@@ -11,3 +11,8 @@ CAMERA_OPENCV_MODEL = "https://dl.google.com/coral/canned_models/all_models.tar.
 
 CAMERA_OPENCV_DEFAULT_MODEL = "mobilenet_ssd_v2_coco_quant_postprocess.tflite"
 CAMERA_OPENCV_DEFAULT_LABEL = "coco_labels.txt"
+
+CAMERA_GSTREAMER_MODEL = "https://dl.google.com/coral/canned_models/all_models.tar.gz"
+
+CAMERA_GSTREAMER_DEFAULT_MODEL = "mobilenet_ssd_v2_coco_quant_postprocess.tflite"
+CAMERA_GSTREAMER_DEFAULT_LABEL = "coco_labels.txt"
