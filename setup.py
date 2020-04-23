1e78baa7f5564832b6fb855a118a63d19235e544
Alifer Moraes <alifer.moraes@nxp.com>
2020-04-23 11:36:17 -0300

    Add recursevely copy function to properly copy files in setup.py
    Signed-off-by: Alifer Moraes <alifer.moraes@nxp.com>

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 import os
-import pathlib
-import shutil
-import sys
 from setuptools import setup, find_packages
+from utils import copy
 
 setup(name="eiq",
 version="1.0",
@@ -14,33 +12,12 @@ license="BDS-3-Clause",
 packages=find_packages(),
 zip_safe=False)
 
-init = "__init__.py"
 demos_dir = os.path.join(os.getcwd(), "eiq", "demos")
 apps_dir = os.path.join(os.getcwd(), "eiq", "apps")
 install_dir_base = os.path.join("/opt", "eiq")
 
-install_dir = os.path.join(install_dir_base, "demos")
+install_dir_demos = os.path.join(install_dir_base, "demos")
+install_dir_apps = os.path.join(install_dir_base, "apps")
 
-if not os.path.exists(install_dir):
-    try:
-        pathlib.Path(install_dir).mkdir(parents=True, exist_ok=True)
-    except OSError:
-        sys.exit("os.mkdir() function has failed: %s" % install_dir)
-
-for file in os.listdir(demos_dir):
-    if file != init:
-        file = os.path.join(demos_dir, file)
-        shutil.copy(file, install_dir)
-
-install_dir = os.path.join(install_dir_base, "apps")
-
-if not os.path.exists(install_dir):
-    try:
-        pathlib.Path(install_dir).mkdir(parents=True, exist_ok=True)
-    except OSError:
-        sys.exit("os.mkdir() function has failed: %s" % install_dir)
-
-for file in os.listdir(apps_dir):
-    if file != init:
-        file = os.path.join(apps_dir, file)
-        shutil.copy(file, install_dir)
+copy(install_dir_demos, demos_dir)
+copy(install_dir_apps, apps_dir)
