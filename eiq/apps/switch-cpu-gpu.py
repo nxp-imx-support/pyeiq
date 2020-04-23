60d198313bf2ff19afe03ef4eed6df3c72494089
Diego Dorta <diego.dorta@nxp.com>
2020-04-23 02:40:14 -0300

    Add initial label image switch demo
    Signed-off-by: Diego Dorta <diego.dorta@nxp.com>

---
+++ eiq/apps/switch-cpu-gpu.py
@@ -0,0 +1,9 @@
+from eiq.apps.parser import run_label_image_no_accel, run_label_image_accel
+
+# include Gtk stuff here.
+
+x = run_label_image_no_accel()
+y = run_label_image_accel()
+
+print(x)
+print(y)
