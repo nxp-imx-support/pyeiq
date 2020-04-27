5cec47485302d506e0d97c71eb6c6fe9064df99e
Diego Dorta <diego.dorta@nxp.com>
2020-04-27 14:14:33 -0300

    Add title as contants to be loaded
    Signed-off-by: Diego Dorta <diego.dorta@nxp.com>

--- eiq/config.py
+++ eiq/config.py
@@ -5,3 +5,11 @@ CHUNK_DEFAULT_SIZE = 32768
 REGULAR_DOWNLOAD_URL = 'https://docs.google.com/uc?export=download'
 
 INIT_MODULE_FILE = "__init__.py"
+
+# Apps Titles
+TITLE_LABEL_IMAGE_SWITCH = "PyeIQ - Label Image Switch App"
+
+# Demos Titles
+TITLE_OBJECT_DETECTION_CAMERA = "PyeIQ - Object Detection Camera"
+TITLE_OBJECT_DETECTION_OPENCV = "PyeIQ - Object Detection OpenCV"
+TITLE_FIRE_DETECTION_CAMERA = "PyeIQ - Fire Detection Camera"
