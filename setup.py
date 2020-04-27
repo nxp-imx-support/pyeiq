eb0e85456f8a97fa2319f98784d5aa731071c98c
Alifer Moraes <alifer.moraes@nxp.com>
2020-04-27 20:50:33 -0300

    Remove /opt/eiq directory when reinstalling eiq package
    Signed-off-by: Alifer Moraes <alifer.moraes@nxp.com>

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ import pathlib
 import shutil
 import sys
 from setuptools import setup, find_packages
+
 from eiq.utils import copy
 
 setup(name="eiq",
@@ -18,20 +19,27 @@ license="BDS-3-Clause",
 packages=find_packages(),
 zip_safe=False)
 
-demos_dir = os.path.join(os.getcwd(), "eiq", "demos")
 apps_dir = os.path.join(os.getcwd(), "eiq", "apps")
+base_dir = os.path.join("/opt", "eiq")
+demos_dir = os.path.join(os.getcwd(), "eiq", "demos")
 switch_label = "eiq/apps/label/switch-label.py"
-install_dir_base = os.path.join("/opt", "eiq")
 
-install_dir_demos = os.path.join(install_dir_base, "demos")
-install_dir_apps = os.path.join(install_dir_base, "apps")
+install_dir_demos = os.path.join(base_dir, "demos")
+install_dir_apps = os.path.join(base_dir, "apps")
+
+if os.path.exists(base_dir):
+    try:
+        print("Removing {0}...".format(base_dir))
+        shutil.rmtree(base_dir)
+    except OSError as e:
+        print("Error: %s : %s" % (base_dir, e.strerror))
 
 copy(install_dir_demos, demos_dir)
 
 if not os.path.exists(install_dir_apps):
     try:
         pathlib.Path(install_dir_apps).mkdir(parents=True, exist_ok=True)
-    except OSError:
-        sys.exit("os.mkdir() function has failed: %s" % install_dir_apps)
+    except OSError as e:
+        sys.exit("pathlib.Path.mkdir() function has failed: %s : %s" % (install_dir_apps, e.strerror))
 
     shutil.copy(switch_label, install_dir_apps)
