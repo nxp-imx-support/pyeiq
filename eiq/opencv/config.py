24ac6920bbdd10d720ad6e62dfc10a3678b01c73
Alifer Moraes <alifer.moraes@nxp.com>
2020-05-04 17:40:46 -0300

    [OpenCV] Add demo to new category of framework
    Add a new demo that uses opencv dnn as inference framework
    
    Signed-off-by: Alifer Moraes <alifer.moraes@nxp.com>

---
+++ eiq/opencv/config.py
@@ -0,0 +1,8 @@
+import os
+
+SSD_OBJ_DETECTION_PROTO = "https://github.com/diegohdorta/models/raw/master/caffe/MobileNetSSD_deploy.prototxt"
+SSD_OBJ_DETECTION_CAFFE = "https://github.com/diegohdorta/models/raw/master/caffe/MobileNetSSD_deploy.caffemodel"
+SSD_OBJ_DETECTION_LABEL_ID = "1VrC68eKofWNzYhTEW3ky7JvYOSSaNqKq"
+SSD_OBJ_DETECTION_DEFAULT_IMAGE = "https://raw.githubusercontent.com/tensorflow/tensorflow/master/tensorflow/lite/examples/label_image/testdata/grace_hopper.bmp"
+
+TMP_DIR = os.path.join("/tmp", "eiq")
