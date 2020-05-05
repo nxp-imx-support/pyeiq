215dd41c9eebec39d1f261af46a0c6347298fa08
Alifer Moraes <alifer.moraes@nxp.com>
2020-05-05 12:19:59 -0300

    UP: tflite: classification: Move class accordingly
    Move class eIQObjectDetection and its configuration from eiq.tflite to
    eiq.tfilte.ssd to better organize the project's structure
    
    Signed-off-by: Alifer Moraes <alifer.moraes@nxp.com>

--- eiq/tflite/config.py
+++ eiq/tflite/config.py
@@ -1,8 +1,6 @@
 # Copyright 2020 NXP Semiconductors
 # SPDX-License-Identifier: BSD-3-Clause
 
-OBJECT_RECOGNITION_MODEL = "https://storage.googleapis.com/download.tensorflow.org/models/tflite/coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip"
-
 LABEL_IMAGE_MODEL = "https://github.com/google-coral/edgetpu/raw/master/test_data/mobilenet_v2_1.0_224_quant.tflite"
 LABEL_IMAGE_LABELS = "https://github.com/google-coral/edgetpu/raw/master/test_data/imagenet_labels.txt"
 LABEL_IMAGE_DEFAULT_IMAGE = "https://raw.githubusercontent.com/tensorflow/tensorflow/master/tensorflow/lite/examples/label_image/testdata/grace_hopper.bmp"
@@ -22,6 +20,5 @@ CAMERA_GSTREAMER_DEFAULT_LABEL = "coco_labels.txt"
 
 # Demos Titles
 
-TITLE_OBJECT_DETECTION_CAMERA = "PyeIQ - Object Detection Camera"
 TITLE_OBJECT_DETECTION_OPENCV = "PyeIQ - Object Detection OpenCV"
 TITLE_FIRE_DETECTION_CAMERA = "PyeIQ - Fire Detection Camera"
--- eiq/tflite/ssd/config.py
+++ eiq/tflite/ssd/config.py
@@ -2,7 +2,15 @@ from os.path import join
 
 TMP_DIR = join('/tmp', 'eiq')
 
+OBJ_DETECTION_CAM_LABEL_NAME = "labelmap.txt"
+OBJ_DETECTION_CAM_MODEL = "https://storage.googleapis.com/download.tensorflow.org/models/tflite/coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip"
+OBJ_DETECTION_CAM_MODEL_NAME = "detect.tflite"
+
 OBJ_DETECTION_SSD_IMAGE = 'https://github.com/kaka-lin/object-detection/raw/master/images/dog.jpg'
 OBJ_DETECTION_SSD_LABEL = 'https://github.com/kaka-lin/object-detection/raw/master/model_data/coco_classes.txt'
 OBJ_DETECTION_SSD_MODEL_ID = '1Ha9yfjkweCatEo6UoZgZyHMeyIBGe5FO'
 OBJ_DETECTION_SSD_MODEL_NAME = 'ssd_mobilenet_v2.tflite'
+
+# Demos Titles
+
+TITLE_OBJECT_DETECTION_CAM = "PyeIQ - Object Detection Camera"
