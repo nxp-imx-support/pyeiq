966d0d631e1b160eda18d8e1d7f802bf823a6d17
Alifer Moraes <alifer.moraes@nxp.com>
2020-05-12 13:55:56 -0300

    UP: eiq: engines: tflite: Add TFLiteInterpreter
    Create a class to deal with everything related to tflite interpreter
    
    Signed-off-by: Alifer Moraes <alifer.moraes@nxp.com>

--- eiq/engines/tflite/inference.py
+++ eiq/engines/tflite/inference.py
@@ -1,6 +1,37 @@
+import numpy as np
 from tflite_runtime.interpreter import Interpreter
 
 from eiq.utils import timeit
+
+class TFLiteInterpreter:
+    def __init__(self, model=None):
+        self.interpreter = None
+        self.input_details = None
+        self.output_details = None
+
+        if model is not None:
+            self.interpreter = Interpreter(model)
+            self.interpreter.allocate_tensors()
+            self.input_details = self.interpreter.get_input_details()
+            self.output_details = self.interpreter.get_output_details()
+
+    def dtype(self):
+        return self.input_details[0]['dtype']
+
+    def get_tensor(self, index, squeeze=False):
+        if squeeze:
+            return np.squeeze(self.interpreter.get_tensor(
+                                   self.output_details[index]['index']))
+
+        return self.interpreter.get_tensor(
+                    self.output_details[index]['index'])
+
+    def set_tensor(self, image):
+        self.interpreter.set_tensor(self.input_details[0]['index'], image)
+
+    def run_inference(self):
+        with timeit("Inference time"):
+            self.interpreter.invoke()
 
 
 def get_details(interpreter):
