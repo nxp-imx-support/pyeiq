7e7a1862c90e1d9356306e013edbe31ece4ccec4
Diego Dorta <diego.dorta@nxp.com>
2020-05-13 23:29:00 -0300

    UP: setup: Add attributes and function to read the README file
    Signed-off-by: Diego Dorta <diego.dorta@nxp.com>

--- setup.py
+++ setup.py
@@ -9,17 +9,13 @@ from setuptools import setup, find_packages
 
 from eiq.utils import copy
 
-setup(name="eiq",
-version="1.0",
-description="eIQ package provides classes and scripts to manage the eIQ Samples Apps.",
-url="",
-author="Alifer Moraes <alifer.moraes@nxp.com>, Diego Dorta <diego.dorta@nxp.com>, Marco Franchi <marco.franchi@nxp.com>",
-author_email="diego.dorta@nxp.com",
-license="BDS-3-Clause",
-packages=find_packages(),
-zip_safe=False)
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
-os.system("pip3 install https://neo-ai-dlr-release.s3-us-west-2.amazonaws.com/v1.1.0/a1-aarch64-ubuntu18_04-glibc2_27-libstdcpp3_4/dlr-1.1.0-py2.py3-none-any.whl")
+os.system("pip3 install https://neo-ai-dlr-release.s3-us-west-2.amazonaws.com/" \
+          "v1.1.0/a1-aarch64-ubuntu18_04-glibc2_27-libstdcpp3_4/" \
+          "dlr-1.1.0-py2.py3-none-any.whl")
 
 apps_dir = os.path.join(os.getcwd(), "eiq", "apps")
 base_dir = os.path.join("/opt", "eiq")
@@ -43,7 +39,27 @@ if not os.path.exists(install_dir_apps):
     try:
         pathlib.Path(install_dir_apps).mkdir(parents=True, exist_ok=True)
     except OSError as e:
-        sys.exit("pathlib.Path.mkdir() function has failed: %s : %s" % (install_dir_apps, e.strerror))
+        sys.exit("pathlib.Path.mkdir() function has failed: %s : %s" %
+                 (install_dir_apps, e.strerror))
 
     shutil.copy(switch_label, install_dir_apps)
     shutil.copy(images_player, install_dir_apps)
+
+setup(name="eiq",
+      version="1.0.0",
+      description="A Python Framework for eIQ on i.MX Processors",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url = 'https://source.codeaurora.org/external/imxsupport/pyeiq/',
+      author="Alifer Moraes, Diego Dorta, Marco Franchi",
+      license="BDS-3-Clause",
+      packages=find_packages(),
+      zip_safe=False,
+      keywords = ['ml', 'eiq', 'demos', 'apps'],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Information Technology',
+        'Natural Language :: English',
+        'Operating System :: Other OS',
+        'Programming Language :: Python :: 3.7'
+      ])
