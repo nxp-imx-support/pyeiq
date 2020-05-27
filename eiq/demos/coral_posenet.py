0476f60724f60768b1eda1f24332b48d8a0e274a
Pedro Jardim <pedro.jardim@nxp.com>
2020-05-27 11:49:44 -0300

    NEW: add coral posenet demo
    Signed-off-by: Pedro Jardim <pedro.jardim@nxp.com>

---
+++ eiq/demos/coral_posenet.py
@@ -0,0 +1,12 @@
+# Copyright 2020 NXP Semiconductors
+# SPDX-License-Identifier: BSD-3-Clause
+
+from eiq.posenet import PoseNet
+
+def main():
+    app = PoseNet()
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
