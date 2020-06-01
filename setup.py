cfd6becbfbccd421fa604e7ebfbd8bd10d7170b3
Diego Dorta <diego.dorta@nxp.com>
2020-06-01 20:05:15 -0300

    UP: Remove neo dlr references
    Signed-off-by: Diego Dorta <diego.dorta@nxp.com>

--- setup.py
+++ setup.py
@@ -13,10 +13,6 @@ this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-os.system("pip3 install https://neo-ai-dlr-release.s3-us-west-2.amazonaws.com/" \
-          "v1.1.0/a1-aarch64-ubuntu18_04-glibc2_27-libstdcpp3_4/" \
-          "dlr-1.1.0-py2.py3-none-any.whl")
-
 apps_dir = os.path.join(os.getcwd(), "eiq", "apps")
 base_dir = os.path.join("/opt", "eiq")
 demos_dir = os.path.join(os.getcwd(), "eiq", "demos")
