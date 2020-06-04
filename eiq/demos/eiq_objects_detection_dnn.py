a72dd588658c058dcfb4c733f7ded14ee4774955
Alifer Moraes <alifer.moraes@nxp.com>
2020-06-04 17:18:49 -0300

    UP: demos: Update demo name
    Update demo name and imported class name
    
    Signed-off-by: Alifer Moraes <alifer.moraes@nxp.com>

--- eiq/demos/eiq_objects_detection_dnn.py
+++ eiq/demos/eiq_objects_detection_dnn.py
@@ -1,11 +1,12 @@
+#!/usr/bin/env python3
 # Copyright 2020 NXP Semiconductors
 # SPDX-License-Identifier: BSD-3-Clause
 
-from eiq.modules.detection.object_detection_ssd import eIQObjectDetectionImage
+from eiq.modules.detection.object_detection_ssd import eIQObjectDetectionDNN
 
 
 def main():
-    app = eIQObjectDetectionImage()
+    app = eIQObjectDetectionDNN()
     app.run()
 
 
