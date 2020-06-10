8717e7018791ab775d071034dbc285ae870cc52d
Alifer Moraes <alifer.moraes@nxp.com>
2020-06-10 16:56:34 -0300

    RM: multimedia: v4l2: Remove unused function
    Signed-off-by: Alifer Moraes <alifer.moraes@nxp.com>

--- eiq/multimedia/v4l2.py
+++ eiq/multimedia/v4l2.py
@@ -1,14 +1,6 @@
 # Copyright 2020 NXP Semiconductors
 # SPDX-License-Identifier: BSD-3-Clause
 
-import os
-
-
-def run():
-    pipeline = "gst-launch-1.0 v4l2src ! autovideosink"
-    os.system(pipeline)
-
-
 def v4l2_camera_pipeline(width, height, device, frate,
                          leaky="leaky=downstream max-size-buffers=1",
                          sync="sync=false emit-signals=true drop=true "\
@@ -18,6 +10,7 @@ def v4l2_camera_pipeline(width, height, device, frate,
              "! queue {} ! videoconvert ! appsink {}").format(device, width,
                                                               height, frate,
                                                               leaky, sync))
+
 
 def v4l2_video_pipeline(device, leaky="leaky=downstream max-size-buffers=1",
                         sync="sync=false drop=True max-buffers=1 "\
