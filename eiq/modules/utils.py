8ab2c1d1c039bfcca8a46175f3de7c7eee94398d
Alifer Moraes <alifer.moraes@nxp.com>
2020-06-11 22:58:37 -0300

    UP: modules: utils: Modify real_time_inference
    Modify the real_time_inference function. Now it deals with both video
    and image displaying instead of only video. This way the image
    displaying routines are no longer repeated in every demo.
    
    Change its name into run_inference to keep consistency with its
    functionality.
    
    Signed-off-by: Alifer Moraes <alifer.moraes@nxp.com>

--- eiq/modules/utils.py
+++ eiq/modules/utils.py
@@ -83,23 +83,30 @@ class GstVideo:
 
         return Gst.FlowReturn.OK
 
-def real_time_inference(inference_func, args):
-    video_config = VideoConfig(args)
-    sink, src = video_config.get_config()
+def run_inference(inference_func, image, args):
+    if args.video_src:
+        video_config = VideoConfig(args)
+        sink, src = video_config.get_config()
 
-    if not src:
-        if (not sink) or (not sink.isOpened()):
-            sys.exit("Your video device could not be initialized. Exiting...")
-        while sink.isOpened():
-            ret, frame = sink.read()
-            if ret:
-                cv2.imshow(*inference_func(frame))
-            else:
-                print("Your video device could not capture any image.")
-                break
-            if (cv2.waitKey(1) & 0xFF) == ord('q'):
-                break
-        sink.release()
+        if not src:
+            if (not sink) or (not sink.isOpened()):
+                sys.exit("Your video device could not be initialized. Exiting...")
+            while sink.isOpened():
+                ret, frame = sink.read()
+                if ret:
+                    cv2.imshow(*inference_func(frame))
+                else:
+                    print("Your video device could not capture any image.")
+                    break
+                if (cv2.waitKey(1) & 0xFF) == ord('q'):
+                    break
+            sink.release()
+        else:
+            gst_video = GstVideo(sink, src, inference_func)
+            gst_video.run()
     else:
-        gst_video = GstVideo(sink, src, inference_func)
-        gst_video.run()
+        frame = cv2.imread(image, cv2.IMREAD_COLOR)
+        cv2.imshow(*inference_func(frame))
+        cv2.waitKey()
+
+    cv2.destroyAllWindows()
