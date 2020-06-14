732b83d544c2a9283d25fa63829ee72896431b50
Alifer Moraes <alifer.moraes@nxp.com>
2020-06-14 19:51:37 -0300

    UP: classification: config: Rewrite constants
    Rewrite constants that will be used within the new DemoBase class,
    following its new standard.
    
    Signed-off-by: Alifer Moraes <alifer.moraes@nxp.com>

--- eiq/modules/classification/config.py
+++ eiq/modules/classification/config.py
@@ -1,36 +1,31 @@
 # Copyright 2020 NXP Semiconductors
 # SPDX-License-Identifier: BSD-3-Clause
 
-FIRE_DETECTION_MEDIA_NAME = "fire.jpg"
-FIRE_DETECTION_MODEL_NAME = "fire_detection.tflite"
-FIRE_DETECTION_MODEL_SHA1 = "2df946680459a3b20bd668f423dcdaa6b76a98b3"
-FIRE_DETECTION_MODEL_SRC = {'drive': "https://drive.google.com/file/d/"
-                                     "1aDsAB2i9wl1xbkexXbiYfPawKUMYXfwH/"
-                                     "view?usp=sharing",
-                            'github': "https://github.com/diegohdorta/"
-                                      "models/raw/master/models/"
-                                      "eIQFireClassification.zip"}
+FIRE_CLASSIFICATION = {'image': "fire.jpg",
+                       'model': "fire_detection.tflite",
+                       'sha1': "2df946680459a3b20bd668f423dcdaa6b76a98b3",
+                       'src': {'drive': "https://drive.google.com/file/d/"
+                                        "1aDsAB2i9wl1xbkexXbiYfPawKUMYXfwH/"
+                                        "view?usp=sharing",
+                               'github': "https://github.com/diegohdorta/"
+                                         "models/raw/master/models/"
+                                         "eIQFireClassification.zip"},
+                       'window_title': "PyeIQ Fire Classification"}
 
-OBJ_CLASSIFICATION_LABEL_NAME = "labels_mobilenet_quant_v1_224.txt"
-OBJ_CLASSIFICATION_MEDIA_NAME = "cat.jpg"
-OBJ_CLASSIFICATION_MODEL_NAME = "mobilenet_v1_1.0_224_quant.tflite"
-OBJ_CLASSIFICATION_MODEL_SHA1 = "765c995e1d27c4a738f77cf13445e7b41306befc"
-OBJ_CLASSIFICATION_MODEL_SRC = {'drive': "https://drive.google.com/file/d/"
-                                           "1yhQkJZwtuSyOvTOKi0ZVcIvZUR7GXv6z/"
-                                           "view?usp=sharing",
-                                  'github': "https://github.com/diegohdorta/"
-                                            "models/raw/master/models/"
-                                            "eIQImageClassification.zip"}
+OBJ_CLASSIFICATION = {'image': "cat.jpg",
+                      'labels': "labels_mobilenet_quant_v1_224.txt",
+                      'model': "mobilenet_v1_1.0_224_quant.tflite",
+                      'sha1': "765c995e1d27c4a738f77cf13445e7b41306befc",
+                      'src': {'drive': "https://drive.google.com/file/d/"
+                                       "1yhQkJZwtuSyOvTOKi0ZVcIvZUR7GXv6z/"
+                                       "view?usp=sharing",
+                              'github': "https://github.com/diegohdorta/"
+                                        "models/raw/master/models/"
+                                        "eIQImageClassification.zip"},
+                      'window_title': "PyeIQ Image Classification"}
 
 CV_GREEN = (0, 255, 0)
 CV_RED = (0, 0, 255)
 
 FIRE = "Fire Detected!"
 NO_FIRE = "No Fire"
-
-ZIP = ".zip"
-
-# Demos's Titles
-
-TITLE_FIRE_CLASSIFICATION = "PyeIQ Fire Classification"
-TITLE_OBJ_CLASSIFICATION = "PyeIQ Image Classification"
