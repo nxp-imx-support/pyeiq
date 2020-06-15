448c7e182df533d9508e2f79c11de854641e6832
Alifer Moraes <alifer.moraes@nxp.com>
2020-06-15 10:39:48 -0300

    UP: multimedia: overlay: Update display_result
    Update the method display_result of the OpenCVOverlay class.
    With the new implementation of load_label method in demo classes,
    class_names_dict is no longer needed.
    
    Signed-off-by: Alifer Moraes <alifer.moraes@nxp.com>

--- eiq/multimedia/overlay.py
+++ eiq/multimedia/overlay.py
@@ -16,8 +16,7 @@ class OpenCVOverlay:
                     (3, 12), cv2.FONT_HERSHEY_SIMPLEX,
                     0.4, (255, 255, 255), 1, cv2.LINE_AA)
 
-    def display_result(self, frame, time, result, labels, colors,
-                       class_names_dict):
+    def display_result(self, frame, time, result, labels, colors):
         width = frame.shape[1]
         height = frame.shape[0]
 
@@ -35,9 +34,6 @@ class OpenCVOverlay:
             bottom = min(height, np.floor(y2 + 0.5).astype('int32'))
             right = min(width, np.floor(x2 + 0.5).astype('int32'))
 
-            cv2.rectangle(frame, (left, top), (right, bottom),
-                          colors[class_names_dict[_id]], 6)
-
             label_size = cv2.getTextSize(labels[_id], FONT, FONT_SIZE,
                                          FONT_THICKNESS)[0]
             label_rect_left = int(left - 3)
@@ -45,9 +41,11 @@ class OpenCVOverlay:
             label_rect_right = int(left + 3 + label_size[0])
             label_rect_bottom = int(top - 5 - label_size[1])
 
+            cv2.rectangle(frame, (left, top), (right, bottom),
+                          colors[int(_id) % len(colors)], 6)
             cv2.rectangle(frame, (label_rect_left, label_rect_top),
                           (label_rect_right, label_rect_bottom),
-                          colors[class_names_dict[_id]], -1)
+                          colors[int(_id) % len(colors)], -1)
             cv2.putText(frame, labels[_id], (left, int(top - 4)),
                         FONT, FONT_SIZE, FONT_COLOR, FONT_THICKNESS)
             self.draw_inference_time(frame, time)
