51c25857508e054144542421cdcef2027068e05b
Alifer Moraes <alifer.moraes@nxp.com>
2020-06-25 10:16:14 -0300

    UP: modules: utils: Add media verification
    Verify and validate image and video_src parameters.
    
    Signed-off-by: Alifer Moraes <alifer.moraes@nxp.com>

--- eiq/modules/utils.py
+++ eiq/modules/utils.py
@@ -11,7 +11,7 @@ import cv2
 from eiq.config import BASE_DIR, ZIP
 from eiq.multimedia.overlay import OpenCVOverlay
 from eiq.multimedia.utils import GstVideo, VideoConfig
-from eiq.utils import args_parser, Downloader, Framerate
+from eiq.utils import args_parser, Downloader, file_type,Framerate
 
 
 class DemoBase:
@@ -43,8 +43,10 @@ class DemoBase:
             downloader.retrieve_data(self.data['src'], self.class_name + ZIP,
                                      self.base_dir, self.data['sha1'], True)
 
+        self.validate_media()
+
         if hasattr(self.args, 'image'):
-            if self.args.image and os.path.isfile(self.args.image):
+            if self.args.image and (self.args.image == 'image'):
                 self.image = self.args.image
         if not self.image and self.data and 'image' in self.data:
             self.image = os.path.join(self.media_dir, self.data['image'])
@@ -62,6 +64,24 @@ class DemoBase:
                 self.model = self.args.model
         if not self.model and self.data and 'model' in self.data:
             self.model = os.path.join(self.model_dir, self.data['model'])
+
+    def validate_media(self):
+        if self.args.image and (not self.args.video_src):
+            media = file_type(self.args.image)
+            if media == "video":
+                sys.exit("Your image parameter seems to be a video file. "
+                         "Try to use -v instead.")
+            elif (not media) or (media != "image"):
+                print("Your image parameter is not a valid image file. "
+                      "Using default image...")
+
+        if self.args.video_src:
+            media = file_type(self.args.video_src)
+            if media == "image":
+                sys.exit("Your video_src parameter seems to be an image file. "
+                         "Try to use -i instead.")
+            elif media and (media != "video"):
+                sys.exit("Your video parameter is not a valid video file.")
 
     @staticmethod
     def load_labels(path):
