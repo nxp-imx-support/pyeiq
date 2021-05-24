fc516e119d54474f4076077ab3b9464e1ea8c846
lizeze-0515 <1287153151@qq.com>
2021-05-23 22:41:42 -0700

    UP: Change version from 2.0.0 to 3.0.0

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name="eiq",
-      version="2.0.0",
+setup(name="PyeIQ",
+      version="3.0.0",
       description="A Python Framework for eIQ on i.MX Processors",
       long_description=long_description,
       long_description_content_type='text/markdown',
--- tests/test_utils.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from eiq.utils import log
-
-def test_log():
-    log("Testing log function")
-    
-    
-    
